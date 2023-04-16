2dbf372b7c8e68487e1cae8a312bce1aad4b7428
rathoreAbhi0208 <awikumar830@gmail.com>
2023-04-16 12:50:04 +0530

    yoloV5 Added

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s] ')
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:  %(message)s: ')
 
 project_name = "signLanguage"
 
