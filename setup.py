2c30243467b78ab64c6c920304deaf71ca104569
rathoreAbhi0208 <awikumar830@gmail.com>
2023-04-16 13:22:09 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='SignLanguage',
+    version='0.0.0',
+    author='abhi',
+    author_email='awikumar830@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
